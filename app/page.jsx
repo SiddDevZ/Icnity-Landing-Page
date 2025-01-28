6c4b9f7ab84a7e7e05f3f4772cb1ab54b1abeada
SiddDevZ <epic.siddarth@gmail.com>
2025-01-29 04:01:24 +0530

    Why Choose US Section

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,8 @@ export default function Home() {
         <NavBar />
         <main className="py-28 max-w-[77rem] mx-auto relative">
           <div className="max-w-3xl z-10">
-            <div className='flex items-center mb-4'>
-              <div className='w-[1.25rem] rounded-full h-2 bg-[#00a2ffc4] mr-2'></div>
+            <div className="flex items-center mb-4">
+              <div className="w-[1.25rem] rounded-full h-2 bg-[#00a2ffc4] mr-2"></div>
               <div className="font-inter text-sm">Our Mission</div>
             </div>
             <h1 className="text-6xl text-transparent font-inter tracking-wide bg-clip-text bg-gradient-to-r from-[#f5f5f5] to-[#91909f]  mb-4 leading-tight">
@@ -18,9 +18,9 @@ export default function Home() {
               Team, Effortlessly.
             </h1>
             <p className="text-lg text-[#b1b1b1] mb-7 max-w-[39.5rem]">
-              Connect with top-tier talent—fast, flexible, and tailored to your needs. 
-              Whether you're hiring for a full-time role or a short-term project, Icnity 
-              empowers you to bring your vision to life.
+              Connect with top-tier talent—fast, flexible, and tailored to your
+              needs. Whether you're hiring for a full-time role or a short-term
+              project, Icnity empowers you to bring your vision to life.
             </p>
             <button className="bg-[#f3f3f3df] text-black font-bold py-3 px-7 rounded-full hover:bg-gray-200 hover:shadow-[0_0_20px_rgba(255,255,255,0.3)] hover:scale-[1.03] transition ease-out duration-300">
               GET STARTED TODAY
@@ -30,72 +30,162 @@ export default function Home() {
             <div className="relative w-full h-full">
               {/* Radial gradient overlay */}
               <div className="absolute inset-0 bg-[#151414] [mask-image:radial-gradient(ellipse_at_center,transparent_5%,#151414)] z-10"></div>
-              
-              <img 
-                src="line.png" 
-                alt="Line background" 
+
+              <img
+                src="line.png"
+                alt="Line background"
                 className="absolute top-1/2 right-0 transform -translate-y-1/2 w-[800px] h-[450px] object-cover object-left"
               />
-              <img 
-                src="hero.png" 
-                alt="Hero image" 
+              <img
+                src="hero.png"
+                alt="Hero image"
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[340px] h-auto object-contain z-20"
               />
             </div>
           </div>
         </main>
 
-        <section className='w-full mb-8 py-10 border-t border-b border-[#252525]'>
-          <div className='max-w-[77rem] mx-auto flex justify-between items-center'>
-            <div className='flex items-center'>
-              <h3 className='text-lg font-medium text-[#f4f4f4] font-inter'>10K+ Community Members</h3>
+        <section className="w-full mb-8 py-10 border-t border-b border-[#252525]">
+          <div className="max-w-[77rem] mx-auto flex justify-between items-center">
+            <div className="flex items-center">
+              <h3 className="text-lg font-medium text-[#f4f4f4] font-inter">
+                10K+ Community Members
+              </h3>
             </div>
-            <img src="star.svg" alt="" className='w-7 h-auto' />
-            <div className='flex items-center'>
-              <h3 className='text-lg font-medium text-[#f4f4f4] font-inter'>1,000+ Collaborators</h3>
+            <img src="star.svg" alt="" className="w-7 h-auto" />
+            <div className="flex items-center">
+              <h3 className="text-lg font-medium text-[#f4f4f4] font-inter">
+                1,000+ Collaborators
+              </h3>
             </div>
-            <img src="star.svg" alt="" className='w-7 h-auto' />
-            <div className='flex items-center'>
-              <h3 className='text-lg font-medium text-[#f4f4f4] font-inter'>98% Client Satisfaction Rate</h3>
+            <img src="star.svg" alt="" className="w-7 h-auto" />
+            <div className="flex items-center">
+              <h3 className="text-lg font-medium text-[#f4f4f4] font-inter">
+                98% Client Satisfaction Rate
+              </h3>
             </div>
           </div>
         </section>
 
-        <section className='about mt-[6rem] max-w-[77rem] mx-auto flex items-center space-x-20'>
-          <div className='w-1/2 relative flex justify-center items-center'>
-            <img 
-              src="round-ring.svg" 
-              alt="Background Ring" 
+        <section className="about mt-[6rem] max-w-[77rem] mx-auto flex items-center space-x-20">
+          <div className="w-1/2 relative flex justify-center items-center">
+            <img
+              src="round-ring.svg"
+              alt="Background Ring"
               className="absolute w-[100%] h-auto z-0"
             />
-            <div 
-              className="w-[65%] h-[25rem] bg-[#1E1E21] rounded-2xl z-10 relative"
-            ></div>
+            <div className="w-[65%] h-[25rem] bg-[#1E1E21] rounded-2xl z-10 relative"></div>
           </div>
-          <div className='w-1/2'>
-            <div className='flex items-center mb-2'>
-              <div className='w-[1.25rem] rounded-full h-2 bg-[#00a2ffc4] mr-2'></div>
+          <div className="w-1/2">
+            <div className="flex items-center mb-2">
+              <div className="w-[1.25rem] rounded-full h-2 bg-[#00a2ffc4] mr-2"></div>
               <div className="font-inter text-sm">About Us</div>
             </div>
             <h2 className="font-inter tracking-wide text-5xl text-[#f5f5f5] mb-1 leading-tight">
-              Passion for Digital<br />Transformation
+              Passion for Digital
+              <br />
+              Transformation
             </h2>
             <p className="text-lg font-inter text-[#b1b1b1] mb-6">
               Empowering innovation, driving change, and shaping a connected
               future through cutting-edge technology and design.
             </p>
             <div className="grid grid-cols-2 gap-4">
-              {['Vision-Driven Innovation', 'Cutting-Edge Technology', 'Community-Centric Approach', 'Passionate Team', 'Comprehensive Solutions', 'Global Impact'].map((item, index) => (
+              {[
+                "Vision-Driven Innovation",
+                "Cutting-Edge Technology",
+                "Community-Centric Approach",
+                "Passionate Team",
+                "Comprehensive Solutions",
+                "Global Impact",
+              ].map((item, index) => (
                 <div key={index} className="flex items-center">
                   <img src="star.svg" alt="" className="w-4 h-4 mr-2" />
                   <span className="text-[#f4f4f4] font-inter">{item}</span>
                 </div>
               ))}
             </div>
-            <button className="bg-[#f3f3f3df] mt-8 text-black font-bold py-[0.69rem] px-6 rounded-full hover:bg-gray-200 hover:shadow-[0_0_20px_rgba(255,255,255,0.3)] hover:scale-[1.03] transition ease-out duration-300">
+            <button className="bg-[#f3f3f3df] mt-8 text-black font-bold py-[0.69rem] px-6 rounded-full hover:bg-[#d6d6d6] hover:shadow-[0_0_20px_rgba(255,255,255,0.3)] hover:scale-[1.025] transition ease-out duration-300">
               KNOW MORE ABOUT US
               <i class="ri-arrow-right-line ml-2"></i>
             </button>
+          </div>
+        </section>
+
+        <section className="mt-[9rem] max-w-[77rem] mx-auto">
+          <h2 className="text-center text-6xl text-transparent font-inter tracking-wide bg-clip-text bg-gradient-to-r from-[#f5f5f5] to-[#91909f] leading-tight mb-16">
+            Our Planned Services
+          </h2>
+          <div className="space-y-6">
+            {[
+              { name: "Community Services", icon: "ri-community-line" },
+              { name: "Digital Information Forum", icon: "ri-discuss-line" },
+              { name: "One-on-One Mentorship", icon: "ri-user-voice-line" },
+              { name: "Project and Assignment Guidance", icon: "ri-task-line" },
+              { name: "Hackathons and Challenges", icon: "ri-code-box-line" },
+              {
+                name: "Digital Marketing and Consultancy",
+                icon: "ri-megaphone-line",
+              },
+              {
+                name: "Industry Collaborations and Talks",
+                icon: "ri-team-line",
+              },
+              {
+                name: "Industry Ready Courses",
+                icon: "ri-graduation-cap-line",
+              },
+              { name: "Job and Internship Portal", icon: "ri-briefcase-line" },
+              { name: "Official Merchandise", icon: "ri-t-shirt-line" },
+            ].map((service, index) => (
+              <div
+                key={index}
+                className="group border-[#2e2e32] hover:border-[#58585f] transition-colors duration-300 py-6 px-8 rounded-lg flex justify-between items-center cursor-pointer border"
+              >
+                {index % 2 !== 0 && (
+                  <div className="flex items-center space-x-3">
+                    <span className="text-xs font-inter text-[#00a2ff] bg-[#00a2ff20] px-2 py-1 rounded-full">
+                      Coming Soon
+                    </span>
+                  </div>
+                )}
+
+                <div className="flex items-center space-x-4">
+                  <i className={`${service.icon} text-[2rem] text-[#00a2ff]`}></i>
+                  <h3 className="text-[2rem] font-inter text-[#f4f4f4] group-hover:text-white transition-colors duration-300">
+                    {service.name}
+                  </h3>
+                  <i
+                    className={`ri-arrow-right-line text-2xl text-[#f4f4f4] group-hover:text-white transition-all duration-300 group-hover:translate-x-1`}
+                  ></i>
+                </div>
+
+                {index % 2 === 0 && (
+                  <div className="flex items-center space-x-3">
+                    <span className="text-xs font-inter text-[#00a2ff] bg-[#00a2ff20] px-2 py-1 rounded-full">
+                      Coming Soon
+                    </span>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <section className="mt-[9rem]">
+          <div className="min-h-[20rem] w-full bg-[#151414] bg-grid-white/[0.05] relative flex items-center justify-center">
+            {/* Radial gradient for the container to give a faded look */}
+            <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-[#151414] [mask-image:radial-gradient(ellipse_at_center,transparent_40%,#151414)]"></div>
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex items-center justify-center mb-4">
+                <div className="w-[1.25rem] rounded-full h-2 bg-[#00a2ffc4] mr-2"></div>
+                <div className="font-inter text-sm">Why Choose Us</div>
+                <div className="w-[1.25rem] rounded-full h-2 bg-[#00a2ffc4] ml-2"></div>
+              </div>
+              <h2 className="text-center text-[3.4rem] font-inter tracking-wide text-[#e5e5e5] leading-[1.1] mb-16">
+                Your Trusted Partner in <br /> Growth and Innovation
+              </h2>
+            </div>
           </div>
         </section>
       </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,9 @@
 const plugin = require('tailwindcss/plugin')
+const svgToDataUri = require("mini-svg-data-uri");
+const colors = require("tailwindcss/colors");
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+
+
 /** @type {import('tailwindcss').Config} */
 
 export default {
@@ -22,6 +27,28 @@ export default {
   plugins: [
     plugin(function({ addVariant }) {
       addVariant('firefox', '@-moz-document url-prefix()');
-    })
+    }),
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="75" height="75" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="38" height="38" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
   ],
 };
